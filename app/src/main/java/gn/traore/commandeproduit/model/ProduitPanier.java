88662f2d194d5ec3235ba7f546c34bf995787795
Ibrahima Sory TRAORE <ibrahima982011@gmail.com>
2019-06-27 17:48:10 +0000

    Ajout de la classe représentant les produits du panier

---
+++ app/src/main/java/gn/traore/commandeproduit/model/ProduitPanier.java
@@ -0,0 +1,27 @@
+package gn.traore.commandeproduit.model;
+
+import java.io.Serializable;
+
+public class ProduitPanier implements Serializable {
+
+    private Produit produit_panier;
+    private int quantite_produit_panier;
+
+    public ProduitPanier() {}
+
+    public Produit getProduit_panier() {
+        return produit_panier;
+    }
+
+    public void setProduit_panier(Produit produit_panier) {
+        this.produit_panier = produit_panier;
+    }
+
+    public int getQuantite_produit_panier() {
+        return quantite_produit_panier;
+    }
+
+    public void setQuantite_produit_panier(int quantite_produit_panier) {
+        this.quantite_produit_panier = quantite_produit_panier;
+    }
+}
